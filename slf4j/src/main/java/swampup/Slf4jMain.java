50e4bb023a1c999ca5fddf05946524a34082d52e
Nadav Cohen <nadavcoh@gmail.com>
2016-05-21 13:08:49 -0700

    Slf4j fails

--- slf4j/src/main/java/swampup/Slf4jMain.java
+++ slf4j/src/main/java/swampup/Slf4jMain.java
@@ -1,9 +1,18 @@
 package swampup;
 
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.LoggerFactory;
+
 public class Slf4jMain {
 
-    public static void main(String[] args) {
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(Slf4jMain.class);
 
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        String msg = "Running app (logging from log4j via slf4j)";
+        logger.info(msg);
+
+        logger.info(new QuoteService().randomQuote());
     }
 
 }
