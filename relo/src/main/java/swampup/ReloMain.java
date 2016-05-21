ad8c1814c48a59ee19b64fb623994668b5ec93ad
Nadav Cohen <nadavcoh@gmail.com>
2016-05-21 13:00:54 -0700

    Relo fails

--- relo/src/main/java/swampup/ReloMain.java
+++ relo/src/main/java/swampup/ReloMain.java
@@ -1,9 +1,19 @@
 package swampup;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
 public class ReloMain {
 
     public static void main(String[] args) {
+        Set<Set<String>> ps = Sets.powerSet(Sets.newHashSet("one", "two"));
 
+        ps.forEach( set -> {
+            System.out.print(Joiner.on(",").join(set));
+            System.out.println("(" + set.size() + ")");
+        });
     }
 
 }
